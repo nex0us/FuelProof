23ecb5f2f6f049fc51e53168847dda1f1017f198
nex0us <65622524+nex0us@users.noreply.github.com>
2024-11-17 12:07:10 -0600

    more queries

--- backend/server.js
+++ backend/server.js
@@ -82,6 +82,56 @@ app.get('/api/mpg_comb', (req, res) => {
     });
 });
 
+app.get('/api/toyota_avg_mpg', (req, res) => {
+    const query = `
+        SELECT AVG("MPG Comb") as "AVG MPG Comb"
+        FROM (
+            SELECT * FROM FEG2021
+            UNION ALL
+            SELECT * FROM FEG2022
+            UNION ALL
+            SELECT * FROM FEG2023
+            UNION ALL
+            SELECT * FROM FEG2024
+        ) AS CombinedTables
+        WHERE Manufacturer = 'TOYOTA';
+        `;
+    
+    db.all(query, [], (err, rows) => {
+        if (err) {
+            res.status(500).send({ error: err.message });
+        } else {
+            res.setHeader('Content-Type', 'application/json');
+            res.json(rows);
+        }
+    });
+});
+
+app.get('/api/comp_avg_mpg', (req, res) => {
+    const query = `
+        SELECT AVG("MPG Comb") as "Comp AVG"
+        FROM (
+            SELECT * FROM FEG2021
+            UNION ALL
+            SELECT * FROM FEG2022
+            UNION ALL
+            SELECT * FROM FEG2023
+            UNION ALL
+            SELECT * FROM FEG2024
+        ) AS CombinedTables
+        WHERE Manufacturer != 'TOYOTA';
+        `;
+    
+    db.all(query, [], (err, rows) => {
+        if (err) {
+            res.status(500).send({ error: err.message });
+        } else {
+            res.setHeader('Content-Type', 'application/json');
+            res.json(rows);
+        }
+    });
+});
+
 // Start server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
